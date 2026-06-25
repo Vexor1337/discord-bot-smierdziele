10f4a0851968044026fe8a82b2a5f0941bef85ef
BuildTools <unconfigured@null.spigotmc.org>
2026-06-25 18:34:14 +0200

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,24 @@
+import com.sun.net.httpserver.HttpServer
 import dev.kord.core.Kord
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
 import features.setupAutoAfk
 import features.setupPokeCommand
+import java.net.InetSocketAddress
 
 suspend fun main() {
+
+    val port = System.getenv("PORT")?.toInt() ?: 10000
+    val server = HttpServer.create(InetSocketAddress(port), 0)
+    server.createContext("/") { exchange ->
+        val response = "Bot is active!".toByteArray()
+        exchange.sendResponseHeaders(200, response.size.toLong())
+        exchange.responseBody.write(response)
+        exchange.responseBody.close()
+    }
+    server.start()
+    println("Serwer HTTP uruchomiony na porcie $port")
+
     val token = System.getenv("DISCORD_TOKEN")
         ?: throw IllegalStateException("DISCORD_TOKEN not found")
 
